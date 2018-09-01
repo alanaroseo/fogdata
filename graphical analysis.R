8209e34b4fab9dbe9764e0f7c6efe2b06ad51efe
Alana Chin <alanaroseo@gmail.com>
2018-08-31 19:07:15 -0700

    better plots

--- graphical analysis.R
+++ graphical analysis.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(ggplot2)
+library("viridis")
 # look at data
 summary(fog)
 
@@ -18,7 +19,66 @@ ggplot(data = fog, aes(x = Minutes, y = MPa_dif )) +
   geom_point(aes(color=tree, shape = Type, alpha = height)) 
 
 ggplot(data = fog, aes(x = Minutes, y = weight_perA )) +
-  geom_point(aes(color=tree, shape = Type, alpha = height)) 
+  geom_point(aes(color=Type, shape = tree, alpha = height)) 
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA )) +
+  geom_point(aes(color=height, shape = tree), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~Type)
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA )) +
+  geom_point(aes(color=height, shape = tree, size = MPa_dif), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~Type)
+
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA )) +
+  geom_point(aes(color=height, shape = Type, size = MPa_dif), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~tree)
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA )) +
+  geom_point(aes(color=MPa_dif, shape = Type), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~tree)
+
+ggplot(data = fog, aes(x = Minutes, y = MPa_dif)) +
+  geom_point(aes(color=height, shape = Type), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~tree)
+
+ggplot(data = fog, aes(x = Minutes, y = MPa_dif)) +
+  geom_point(aes(color=height), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~Type)
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA)) +
+  geom_point(aes(color=height), alpha = .7)+
+  scale_color_viridis()+
+  theme_classic()+
+  facet_wrap(~Type)
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA)) +
+  geom_jitter(aes(color=tree), alpha = .7)+
+  theme_classic()+
+  facet_wrap(~Type)
+
+ggplot(data = fog, aes(x = Minutes, y = weight_perA)) +
+  geom_jitter(aes(color=Type), alpha = .7,  )+
+  theme_classic()+
+  facet_wrap(~tree)
+
+ggplot(data = fog, aes(x = Minutes, y = MPa_perA)) +
+  geom_jitter(aes(color=Type), alpha = .7,  )+
+  theme_classic()+
+  facet_wrap(~tree)
+
 
 #boxplot
 
