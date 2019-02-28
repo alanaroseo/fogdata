b80988a39c739c3bd5a6a12bf46318d9bafcefb7
Alana Chin <alanaroseo@gmail.com>
2019-02-27 16:04:57 -0800

    more gl

--- GL breakdown.R
+++ GL breakdown.R
@@ -8,13 +8,24 @@ library("viridis")
 names(GL)
 
 ggplot(data = GL, aes(x = rel_ht, y = percent_G )) +
-  geom_point(aes(color = tree), alpha = .7)+
+  geom_point( alpha = .7)+
   theme_classic()+
-  geom_smooth(method=lm, se=FALSE, color = "orchid")+
+  geom_smooth(method=lm, se=FALSE, aes(color = site))+
+  facet_wrap(~forest)
+
+ggplot(data = GL, aes(x = branch_ht, y = percent_G )) +
+  geom_point( aes(color = site),alpha = .7)+
+  theme_classic()+
+  geom_smooth(method=lm, se=FALSE, aes(color = site))+
   facet_wrap(~forest)
 
 ggplot(data = GL, aes(x = rel_ht, y = percent_G )) +
-  geom_boxplot(aes(color= branch_ht))+
+  geom_boxplot(aes(color= site,group=site), alpha = .3)+
   theme_classic()+
   facet_wrap(~forest)
 
+plot(percent_G~forest, data =GL)
+
+t.test(percent_G~forest, data =GL)
+
+plot(percent_G~site, data =GL)
