01289c78eb6839f531e6dc50415a9956a9a575e6
Alana Chin <alanaroseo@gmail.com>
2019-12-22 17:27:42 -0800

    m k

--- scripts/surface plot script TT collapse.R
+++ scripts/surface plot script TT collapse.R
@@ -25,12 +25,13 @@ VPD <-  seq(.5,1.5,length=30)#could use by= instead of length= to specify increm
 
 ##### Generate Full range of gs data #####
 
-gs <-  seq(.8,1.8,length=length(VPD))# the same length as the VPD vector
+gs <-  seq(.03,.06,length=length(VPD))# the same length as the VPD vector
+#mean treetop max gs:0.1, mid:0.08, lower:0.06
 
 
 ####### calculate seconds of transpiration sustained
 
-V_released=15 #constant representing mean voulume released during TT collapse (mol*m^-2):
+V_released=0.19 #constant representing mean volume released during TT collapse (mol*m^-2):
 #very highest 5 sampes: 0.06, all above 100:0.13, 70-82m: 0.27, lowest 6 samples (to catch bottom of crown for all trees):0.19.
 
 grid <- function(gs, VPD){
@@ -38,17 +39,12 @@ grid <- function(gs, VPD){
 }#the function "grid" is calculates the # of seconds of transpiration suatainable 
 seconds <- outer(gs,VPD,grid)#outer() function applies the function "grid" at every combination of gs and VPD. Seconds is the z axis (a matrix)
 
-###### the plot
+###### the plots
 
-persp(gs,VPD,seconds,theta = 50, phi = 15,col = "springgreen", shade = 0.5,ticktype = "detailed")#the plot
+persp(gs,VPD,seconds,theta = 50, phi = 15,col = "springgreen", shade = 0.5,ticktype = "detailed")#oldschool
 
-persp3D(gs,VPD,seconds,theta = 50, phi = 30,colvar = seconds, shade = 0.5,ticktype = "detailed", xlab = "gs",ylab = "VPD", zlab = "seconds",colkey = TRUE, facets = FALSE)
+persp3D(gs,VPD,seconds,theta = 40, phi = 30,colvar = seconds, shade = 0.5,ticktype = "detailed", xlab = "gs",ylab = "VPD increase", zlab = "seconds",colkey = TRUE, facets = FALSE)
 
 persp3D(gs,VPD,seconds,theta = 50, phi = 30,colvar = seconds, shade = 0.5,ticktype = "detailed", xlab = "gs",ylab = "VPD", zlab = "seconds",colkey = TRUE,border = "white")
 
-surf3D(seconds, gs, VPD, colvar = seconds, colkey = TRUE, box = FALSE)
-
-wireframe(seconds)
-require(rgl)
-surface3d(gs, VPD, seconds)#require(rgl)
  
